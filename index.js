3c7d8ffa5df7667d8500c681dc0163995799d300
DES <yadavdes7@gmail.com>
2023-12-19 17:48:29 +0545

    basic

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ let NAME = "DES";
 NAME = "ROJALL";
 console.log(NAME);
 
-var NAMEE = "rahul";
-console.log(NAMEE);
+var per = 3.5;
+console.log(per);
 
 const pie = 3.1415;
+const p = 3.1;
 
 console.log(pie);
+console.log(p);
