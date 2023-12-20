c86087a0095e9451b1ae0526df74a114060216b7
DES <yadavdes7@gmail.com>
2023-12-20 17:20:45 +0545

    operations

--- variables-interactions/index.js
+++ variables-interactions/index.js
@@ -8,9 +8,14 @@ const p = 3.1;
 
 console.log(pie);
 console.log(p);
-
+//Interactions
 alert("BEWARE");
+//interaction
 const form = prompt("what is your name");
 
+alert("DO NOT ENTER");
+
+alert("DO NOT ENTER");
+//Interaction (works in a way of true or false)
 const decision = confirm("Are you sure ??");
 console.log(form, decision);
