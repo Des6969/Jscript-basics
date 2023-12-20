8bafe6a1bbd8766115b1e0b2d8680743baf59071
DES <yadavdes7@gmail.com>
2023-12-20 18:01:11 +0545

    statement

---
+++ operations-statements/operation.js
@@ -0,0 +1,22 @@
+console.log("Hello");
+
+const num1 = Number(prompt("enter a number "));
+const num2 = Number(prompt("enter a number"));
+/*
+if (num1 > num2) {
+  alert(num1 * num2);
+} else {
+  alert(Number(num1 + num2));
+}
+
+if (num1 > 5 || num2 == 6) {
+  alert("aayo");
+} else {
+  alert("aayana");
+}
+*/
+if (num1 > 5 && num2 == 6) {
+  alert("aayo");
+} else {
+  alert("aayana");
+}
