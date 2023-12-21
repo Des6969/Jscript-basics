b8a5c11f10f865a903a49903b621c4b09e157904
DES <yadavdes7@gmail.com>
2023-12-21 13:31:58 +0545

    Switch_case

--- operations-statements/statement.js
+++ operations-statements/statement.js
@@ -1,1 +1,33 @@
 console.log("Statement");
+
+const Num = Number(prompt("enter a number "));
+const operator = prompt("enter the opertaion");
+const Num2 = Number(prompt("Enter another number")); //converts the string into number
+
+let Result;
+switch (operator) {
+  case "+":
+    Result = Num + Num2;
+    break;
+  case "-":
+    Result = Num - Num2;
+    break;
+  case "*":
+    Result = Num * Num2;
+    break;
+  case "%":
+    Result = Num % Num2;
+    break;
+  case "/":
+    if (Num2 != 0) {
+      Result = Num / Num2;
+    } else {
+      alert("Not divisible by 0");
+    }
+
+    break;
+
+  default:
+    "Enter valid value";
+}
+console.log("Result:", Result);
