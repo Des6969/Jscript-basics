8dbcd4d6fbd3c2db4cc82482ea12f6799baef542
DES <yadavdes7@gmail.com>
2023-12-21 18:11:08 +0545

    Loop

---
+++ loops/loop.js
@@ -0,0 +1,16 @@
+console.log("Loops");
+
+// for (let a = 1; a <= 100; a++) {
+//   console.log(a);
+// }
+const a = Number(prompt("enter a number"));
+// for (let a = 1; a <= 10; a++) {
+//   console.log(a);
+// }
+
+let i = 1;
+while (i <= 10) {
+  const result = a * i;
+  console.log(result);
+  i++;
+}
--- operations-statements/operation.js
+++ operations-statements/operation.js
@@ -3,20 +3,20 @@ console.log("Hello");
 const num1 = Number(prompt("enter a number "));
 const num2 = Number(prompt("enter a number"));
 
-if (num1 > num2) {
-  alert(num1 * num2);
-} else {
-  alert(Number(num1 + num2));
-}
+// if (num1 > num2) {
+//   alert(num1 * num2);
+// } else {
+//   alert(Number(num1 + num2));
+// }
 
-if (num1 > 5 || num2 == 6) {
-  alert("aayo");
-} else {
-  alert("aayana");
-}
+// if (num1 > 5 || num2 == 6) {
+//   alert("aayo");
+// } else {
+//   alert("aayana");
+// }
 
-if (num1 > 5 && num2 == 6) {
-  alert("aayo");
-} else {
-  alert("aayana");
-}
+// if (num1 > 5 && num2 == 6) {
+//   alert("aayo");
+// } else {
+//   alert("aayana");
+// }
