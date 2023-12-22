a0e2e952a0734a3145e175087ca213cb63196776
DES <yadavdes7@gmail.com>
2023-12-22 17:44:29 +0545

    function

---
+++ functions/index.js
@@ -0,0 +1,46 @@
+console.log("Functions");
+const a = Number(prompt("enter the value "));
+// const b = Number(prompt("enter the value "));
+
+// //defining a function
+// function add(a, b) {
+//   //j kam garirya chac tesko lagi specific function banaune ani name garne
+//   return a + b;
+// }
+// //calling function
+// const o = add(a, b);
+// console.log(o);
+
+// function substract(a, b) {
+//   return a - b;
+// }
+// const o = substract(a, b);
+// console.log(o);
+
+// function multiplication(a, b) {
+//   return a * b;
+// }
+// const o = multiplication(a, b);
+// console.log(o);
+
+// function division(a, b) {
+//   return a / b;
+// }
+// const o = division(a, b);
+// console.log(o);
+
+// function checkEven(a) {
+//   return a % 2 === 0;
+// }
+// const o = checkEven(a);
+// console.log(o);
+// alert("enter even numbeer");
+
+function checkOdd(a) {
+  return a % 2 != 0;
+}
+
+const o = checkOdd(a);
+console.log(o);
+
+alert(o);
