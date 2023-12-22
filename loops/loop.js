78f11aecf90f525041662f55653d6247f6138f28
DES <yadavdes7@gmail.com>
2023-12-22 14:26:56 +0545

    loops

--- loops/loop.js
+++ loops/loop.js
@@ -4,13 +4,18 @@ console.log("Loops");
 //   console.log(a);
 // }
 const a = Number(prompt("enter a number"));
-// for (let a = 1; a <= 10; a++) {
-//   console.log(a);
-// }
-
+//Using for loop
+for (let a = 1; a <= 10; a++) {
+  console.log(a);
+}
+//using while loop
 let i = 1;
 while (i <= 10) {
   const result = a * i;
   console.log(result);
   i++;
 }
+//using Do while loop
+do console.log(result);
+while (i <= 10);
+const result = a * i;
