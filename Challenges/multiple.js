f5e70434c586dc8cb6a7425891964dc3671c34b6
DES <yadavdes7@gmail.com>
2023-12-23 20:29:54 +0545

    Multiple

--- Challenges/multiple.js
+++ Challenges/multiple.js
@@ -5,3 +5,9 @@ const num = Number(prompt("enter a number"));
 if (num % 3 === 0 || num % 7 === 0) {
   alert("Num is a multiple of 3 or 7");
 }
+const multiple =
+  num % 3 === 0
+    ? console.log("Number is multiple of 3")
+    : num % 7 === 0
+    ? console.log("Number is a multiple of &")
+    : console.log("Number is neither a multiple of 3 or 7");
