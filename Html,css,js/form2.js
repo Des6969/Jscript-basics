13a471a7037c24b2e09479db87087860236c12f4
DES <yadavdes7@gmail.com>
2024-01-10 12:27:39 +0545

    form2

---
+++ Html,css,js/form2.js
@@ -0,0 +1,8 @@
+function getFormData() {
+  const mail = document.getElementById("mail").value;
+  const name = document.getElementById("name").value;
+  const college = document.getElementById("college").value;
+  const Password = document.getElementById("pass").value;
+  console.log({ mail, name, college, Password });
+  console.log("Hello");
+}
